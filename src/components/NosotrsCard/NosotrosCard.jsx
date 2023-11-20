7d115c1c7d194831f5007474f61ee5ce870bf871
Santiago Duré <santidure2002@gmail.com>
2023-11-20 20:11:49 -0300

    responsive de pagina servicios listo

--- src/components/NosotrsCard/NosotrosCard.jsx
+++ src/components/NosotrsCard/NosotrosCard.jsx
@@ -17,9 +17,13 @@ function NosotrosCard({ image, title, paragraph, izq }) {
       ) : (
         <div className="nosotrosCard__container">
           <div className="nosotrosCard__content__der">
-            <h3 className="nosotrosCard__title">{title}</h3>
+            <h3 className="nosotrosCard__title nosotrosCard__title__der">
+              {title}
+            </h3>
             <hr className="nosotrosCard__hr" />
-            <p className="nosotrosCard__paragraph">{paragraph}</p>
+            <p className="nosotrosCard__paragraph nosotrosCard__paragraph__der">
+              {paragraph}
+            </p>
           </div>
           <div className="nosotrosCard__img__container">
             <img src={image} alt={"Técnico reparando"} />
